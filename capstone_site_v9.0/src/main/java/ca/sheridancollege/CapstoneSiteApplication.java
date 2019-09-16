3ca0ad769112e7b1efbc56c02344ac8b1779a2de
Jessica Sjostrom <merseine_echo@hotmail.com>
2019-09-16 19:40:07 -0400

    Changes to POM and main java file to be able to create WAR file

--- capstone_site_v9.0/src/main/java/ca/sheridancollege/CapstoneSiteApplication.java
+++ capstone_site_v9.0/src/main/java/ca/sheridancollege/CapstoneSiteApplication.java
@@ -2,9 +2,10 @@ package ca.sheridancollege;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CapstoneSiteApplication {
+public class CapstoneSiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneSiteApplication.class, args);
