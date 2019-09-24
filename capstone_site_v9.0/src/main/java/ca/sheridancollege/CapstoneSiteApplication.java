ea30a33350278a10deb7cbc25596392fe7cd9ea3
Yassin <necroxx206@gmail.com>
2019-09-23 21:49:49 -0400

    testing for deployment

--- capstone_site_v9.0/src/main/java/ca/sheridancollege/CapstoneSiteApplication.java
+++ capstone_site_v9.0/src/main/java/ca/sheridancollege/CapstoneSiteApplication.java
@@ -2,12 +2,18 @@ package ca.sheridancollege;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CapstoneSiteApplication {
+public class CapstoneSiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneSiteApplication.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CapstoneSiteApplication.class);
+    }
 }
