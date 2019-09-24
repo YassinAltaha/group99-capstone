caae24f6537df8fde86adfcb1428f99ec780c642
Jessica Sjostrom <merseine_echo@hotmail.com>
2019-09-23 21:58:56 -0400

    Testing some deployment configurations

--- capstone_site_v9.0/src/main/java/ca/sheridancollege/CapstoneSiteApplication.java
+++ capstone_site_v9.0/src/main/java/ca/sheridancollege/CapstoneSiteApplication.java
@@ -2,9 +2,17 @@ package ca.sheridancollege;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class CapstoneSiteApplication {
+public class CapstoneSiteApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CapstoneSiteApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneSiteApplication.class, args);
